15efe83ef6fd4fa568f7edbbb77bd0681749b985
PinoyTech <64101+pinoytech@users.noreply.github.com>
2026-02-01 18:59:30 +0800

    Add route for ninjas index view

---
+++ resources/views/ninjas/index.blade.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Ninja network | Home</title>
+</head>
+<body>
+  <h2>Current available ninjas</h2>
+  <ul>
+    <li>Ninja 1</li>
+    <li>Ninja 2</li>
+    <li>Ninja 3</li>
+  </ul>
+</body>
+</html>
